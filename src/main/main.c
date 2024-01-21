98bfb09ad7e6e4cbf582a4a161df8d7cf4b19fa3
Justin Fung <qf28@sussex.ac.uk>
2024-01-21 22:20:47 +0000

    Display buttons

--- src/main/main.c
+++ src/main/main.c
@@ -54,10 +54,10 @@ void flush_cb_3ds(lv_disp_drv_t * disp, const lv_area_t * area, lv_color_t * col
     writePic2FrameBuf565(
         gfxGetFramebuffer(GFX_BOTTOM, GFX_LEFT, NULL, NULL),
         color_p,
-        area->x1,
-        area->y1,
-        area->x2 - area->x1,
-        area->y2 - area->y1
+        0,
+        0,
+        WIDTH_BTM,
+        HEIGHT_BTM
     );
 
     // These three lines swap and display new buffer on screen
@@ -68,14 +68,67 @@ void flush_cb_3ds(lv_disp_drv_t * disp, const lv_area_t * area, lv_color_t * col
     lv_disp_flush_ready(disp);         /* Indicate you are ready with the flushing*/
 }
 
-void lv_example_spinner_3ds(void)
-{
-    /*Create a spinner*/
-    lv_obj_t * spinner = lv_spinner_create(lv_scr_act(), 60, 60);
-    lv_obj_set_size(spinner, 50, 50);
-    lv_obj_center(spinner);
-}
+// static void anim_x_cb(void * var, int32_t v)
+// {
+//     lv_obj_set_x(var, v);
+// }
 
+// static void anim_size_cb(void * var, int32_t v)
+// {
+//     lv_obj_set_size(var, v, v);
+// }
+
+// /**
+//  * Create a playback animation
+//  */
+// void lv_example_anim_2(void)
+// {
+
+//     lv_obj_t * obj = lv_obj_create(lv_scr_act());
+//     lv_obj_set_style_bg_color(obj, lv_palette_main(LV_PALETTE_RED), 0);
+//     lv_obj_set_style_radius(obj, LV_RADIUS_CIRCLE, 0);
+
+//     lv_obj_align(obj, LV_ALIGN_LEFT_MID, 10, 0);
+
+//     lv_anim_t a;
+//     lv_anim_init(&a);
+//     lv_anim_set_var(&a, obj);
+//     lv_anim_set_values(&a, 10, 50);
+//     lv_anim_set_time(&a, 1000);
+//     lv_anim_set_playback_delay(&a, 100);
+//     lv_anim_set_playback_time(&a, 300);
+//     lv_anim_set_repeat_delay(&a, 500);
+//     lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+//     lv_anim_set_path_cb(&a, lv_anim_path_ease_in_out);
+
+//     lv_anim_set_exec_cb(&a, anim_size_cb);
+//     lv_anim_start(&a);
+//     lv_anim_set_exec_cb(&a, anim_x_cb);
+//     lv_anim_set_values(&a, 10, 240);
+//     lv_anim_start(&a);
+// }
+
+// void lv_example_style_13(void)
+// {
+//     static lv_style_t style_indic;
+//     lv_style_init(&style_indic);
+//     lv_style_set_bg_color(&style_indic, lv_palette_lighten(LV_PALETTE_RED, 3));
+//     lv_style_set_bg_grad_color(&style_indic, lv_palette_main(LV_PALETTE_RED));
+//     lv_style_set_bg_grad_dir(&style_indic, LV_GRAD_DIR_HOR);
+
+//     static lv_style_t style_indic_pr;
+//     lv_style_init(&style_indic_pr);
+//     lv_style_set_shadow_color(&style_indic_pr, lv_palette_main(LV_PALETTE_RED));
+//     lv_style_set_shadow_width(&style_indic_pr, 10);
+//     lv_style_set_shadow_spread(&style_indic_pr, 3);
+
+//     /*Create an object with the new style_pr*/
+//     lv_obj_t * obj = lv_slider_create(lv_scr_act());
+//     lv_obj_add_style(obj, &style_indic, LV_PART_INDICATOR);
+//     lv_obj_add_style(obj, &style_indic_pr, LV_PART_INDICATOR | LV_STATE_PRESSED);
+//     lv_slider_set_value(obj, 70, LV_ANIM_OFF);
+//     lv_obj_center(obj);
+// }
 
 int main(int argc, char** argv)
 {
@@ -92,8 +145,8 @@ int main(int argc, char** argv)
 
     // Draw buffer
     static lv_disp_draw_buf_t draw_buf;
-    static lv_color_t buf1[WIDTH_BTM * HEIGHT_BTM * 2];
-    lv_disp_draw_buf_init(&draw_buf, buf1, NULL, WIDTH_BTM * HEIGHT_BTM * 2);
+    static lv_color_t buf1[WIDTH_BTM * HEIGHT_BTM];
+    lv_disp_draw_buf_init(&draw_buf, buf1, NULL, WIDTH_BTM * HEIGHT_BTM);
     
     static lv_disp_drv_t disp_drv;        /*Descriptor of a display driver*/
     lv_disp_drv_init(&disp_drv);          /*Basic initialization*/
@@ -105,8 +158,10 @@ int main(int argc, char** argv)
 
 
     // Examples
-    // lv_example_btnmatrix_2();
-    lv_example_spinner_3ds();
+    lv_example_btnmatrix_2();
+    // lv_example_style_13();
+    // lv_example_spinner_3ds();
+    // lv_example_anim_2();
     
 
     /* Display init */
